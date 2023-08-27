018e3bf05695d9513170696fd17ecfa291b456fa
Novanna <novannarahmazani@gmail.com>
2023-08-28 03:00:01 +0700

    initial

---
+++ week1/counterLetters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    int nc = 0;
+
+    printf("Enter a string of characters (press Enter when done):\n");
+
+    while ((c = getchar()) != '\n') {
+        nc++;
+    }
+
+    printf("Number of characters: %d\n", nc);
+
+    return 0;
+}
