9718e6d4a453a7291bf11d44923c88452196e0c1
Novanna <63068113+Novanna@users.noreply.github.com>
2023-10-08 09:24:29 +0700

    Update tk3_soal2.c

--- week7/tk3_soal2.c
+++ week7/tk3_soal2.c
@@ -1,1 +1,109 @@
-cc
+#include <stdio.h>
+#include <string.h>
+
+// Struktur untuk menyimpan data pegawai
+struct Pegawai {
+    char nip[20];
+    char nama[50];
+    char alamat[100];
+    char no_hp[15];
+    char jabatan[50];
+    char golongan[3];
+};
+
+int invalidMsg() {
+    printf("\n==============================================================\n");
+    printf("\t\tERROR\t: Golongan tidak valid");
+    printf("\n==============================================================\n");
+}
+
+// Fungsi untuk menghitung gaji berdasarkan golongan
+int hitungGajiPokok(char golongan[3]) {
+    if (strcmp(golongan, "D1") == 0) {
+        return 2500000;
+    } else if (strcmp(golongan, "D2") == 0) {
+        return 2000000;
+    } else if (strcmp(golongan, "D3") == 0) {
+        return 1500000;
+    } else {
+        invalidMsg();
+        return 1;
+    }
+}
+
+// Fungsi untuk menghitung gaji lemburan
+int hitungGajiLembur(char golongan[3], int jamLembur) {
+    if (strcmp(golongan, "D1") == 0) {
+        return 10000 * jamLembur;
+    } else if (strcmp(golongan, "D2") == 0) {
+        return 5000 * jamLembur;
+    } else if (strcmp(golongan, "D3") == 0) {
+        return 2500 * jamLembur;
+    } else {
+        invalidMsg();
+        return 1;
+    }
+}
+
+int main() {
+    struct Pegawai pegawai;
+    
+    printf("==============================================================\n");
+    printf("PROGRAM MENGHITUNG GAJI BERDASARKAN GOLONGAN DAN GAJI LEMBURAN\n");
+    printf("==============================================================\n");
+    
+    // Input 1
+    printf(">\tMenghitung Gaji Berdasarkan Golongan\t<\n\n");
+    printf("NIP\t\t\t: ");
+    scanf("%s", pegawai.nip);
+    printf("Nama\t\t: ");
+    scanf("%s", pegawai.nama);
+    printf("Alamat\t\t: ");
+    scanf("%s", pegawai.alamat);
+    printf("No HP\t\t: ");
+    scanf("%s", pegawai.no_hp);
+    printf("Jabatan\t\t: ");
+    scanf("%s", pegawai.jabatan);
+    printf("Golongan\t: ");
+    scanf("%s", pegawai.golongan);
+
+    // Process: Hitung gaji pokok
+    int gajiPokok = hitungGajiPokok(pegawai.golongan);
+    
+    // Output 1
+    printf("\n>\tInformasi Gaji Berdasarkan Golongan\t<\n\n");
+    printf("NIP\t\t\t: %s\n", pegawai.nip);
+    printf("Nama\t\t: %s\n", pegawai.nama);
+    printf("Alamat\t\t: %s\n", pegawai.alamat);
+    printf("No HP\t\t: %s\n", pegawai.no_hp);
+    printf("Jabatan\t\t: %s\n", pegawai.jabatan);
+    printf("Golongan\t: %s\n", pegawai.golongan);
+    printf("Gaji\t\t: %d\n\n", gajiPokok);
+
+    printf("==============================================================\n");
+
+    // Input 2
+    int jamLembur;
+    printf(">\tMenghitung Gaji Lemburan\t<\n\n");
+    printf("NIP\t\t\t\t: ");
+    scanf("%s", pegawai.nip);
+    printf("Golongan\t\t: ");
+    scanf("%s", pegawai.golongan);
+    printf("Jumlah Jam Lembur\t: ");
+    scanf("%d", &jamLembur);
+
+    // Hitung gaji lemburan
+    int gajiLembur = hitungGajiLembur(pegawai.golongan, jamLembur);
+
+    // Output 2
+    printf("\n>\tHasil Perhitungan Gaji Lemburan\t<\n\n");
+    printf("NIP\t\t\t: %s\n", pegawai.nip);
+    printf("Golongan\t\t: %s\n", pegawai.golongan);
+    printf("Lembur\t\t\t: %d\n", jamLembur);
+    printf("Total Gaji Bulan ini\t: %d\n", gajiPokok + gajiLembur);
+    
+    printf("==============================================================\n");
+
+
+    return 0; // Exit program dengan sukses
+}
